8fb076bf45900235f214f4b33f2b768ac8e0a100
Ron Sedee <roncd@hotmail.com>
2023-07-07 14:34:11 +0200

    javascript 4

---
+++ Codevoorbeelden/H04/script4-4.js
@@ -0,0 +1,19 @@
+i = 8;
+while(i > 0) {
+    document.write(i + '<br>');
+    i--;
+}
+i = 1;
+while(i <= 99) {
+    i /= 1.5;
+    document.write(i + '<br>');
+    i = i*2;
+}
+var nummer = 0;
+while(nummer <=100) {
+    if (nummer === 77) {
+        document.write(nummer + '<br>');
+        break;
+    }
+    nummer++;
+}
